153a37643d9618870ec3f078db902f904949c2f8
Josh <Joshua.horgen@gmail.com>
2019-09-06 14:11:20 -0700

    Add styles and alignment

--- src/components/UserNav.js
+++ src/components/UserNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Nav, Button, Navbar } from 'react-bootstrap'
 import SiteLogo from './NavLogo';
+import NavBarSelection from './NavBarSelection'
 
 function UserNav(){
 
@@ -8,11 +9,13 @@ function UserNav(){
     backgroundColor: "coral"
   }
 
+
   return (
     <Navbar style={myStyle} expand="lg">
       <Navbar.Brand href="#home"><SiteLogo/></Navbar.Brand>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
+        <NavBarSelection/>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
